1b346efb522dd9cedfae5d90d8a9becda4382395
유노 <yh10207@naver.com>
2017-01-03 22:35:00 +0900

    소스트리 커밋 테스트

--- app/src/main/java/mobile/meatproject/com/meatproject/MainActivity.java
+++ app/src/main/java/mobile/meatproject/com/meatproject/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
         ipNum = (EditText) findViewById(R.id.ipNum);
         point = (TextView) findViewById(R.id.point);
